27254ca5e1baeb6f04b16eab59bd8327a6659e8d
Berita-Winnie <beritawinnie@gmail.com>
2025-10-30 10:05:03 +0300

    (chore) : set up assets.js and ase app struscture

---
+++ src/assets/assets.js
@@ -0,0 +1,130 @@
+import American_girl from './American_girl.png'
+import Apple from './Apple.png'
+import art_direction from './art_direction.png'
+import Awwwards from './Awwwards.png'
+import behance_socials from './behance_socials.png'
+import Behance from './Behance.png'
+import discord from './discord.png'
+import Dribble from './Dribble.png'
+import dribble_socials from './dribble_socials.png'
+import free_bird from './free_bird.png'
+import facebook from './facebook.png'
+import gema_nolen from './gema_nolen.png'
+import gema_nolen1 from './gema_nolen1.png'
+import gema_nolen2 from './gema_nolen2.png'
+import google1 from './google1.png'
+import google from './google.png'
+import Hero_image from './Hero_image.png'
+import Logo from './Logo.png'
+import Menu from './Menu.png'
+import nstagram from './nstagram.png'
+import pablo from './pablo.png'
+import product_design from './product_design.png'
+import purple_haze from './purple_haze.png'
+import Star from './Star.png'
+import under_pressure from './under_pressure.png'
+import visual_design from './visual_design.png'
+import whole_loffa from './whole_loffa.png'
+import You_really_got_me from 'You_really_got_me.png'
+
+export const assets = {
+  American_girl,
+  Apple,
+  art_direction,
+  Awwwards,
+  behance_socials,
+  Behance,
+  discord,
+  Dribble,
+  dribble_socials,
+  free_bird,
+  facebook,
+  gema_nolen,
+  gema_nolen1,
+  gema_nolen2,
+  google1,
+  google,
+  Hero_image,
+  Logo,
+  Menu,
+  nstagram,
+  pablo,
+  product_design,
+  purple_haze,
+  Star,
+  under_pressure,
+  visual_design,
+  whole_loffa,
+  You_really_got_me,
+}
+
+export const companyLogos = [Behance, google, Apple, Dribble, Awwwards]
+
+export const workType = [
+  {
+    image: product_design,
+    title: 'Product design',
+    description:
+      'This template is a figma file .turned into code using Anima.Learn more at AnimaApp.com',
+  },
+  {
+    image: art_direction,
+    title: 'Art direction',
+    description:
+      'This template is a figma file .turned into code using Anima.Learn more at AnimaApp.com',
+  },
+  {
+    image: visual_design,
+    title: 'Visual design',
+    description:
+      'This template is a figma file .turned into code using Anima.Learn more at AnimaApp.com',
+  },
+]
+
+export const Testimonials = [
+  {
+    text: 'This is a template Figma file, turned into code using Anima.Learn more at AnimaApp.com',
+    image: gema_nolen,
+    name: ' Gemi Nolen',
+    company: 'Google',
+  },
+  {
+    text: 'This is a template Figma file, turned into code using Anima.Learn more at AnimaApp.com',
+    image: gema_nolen2,
+    name: ' Gemi Nolen',
+    company: 'Google',
+  },
+  {
+    text: 'This is a template Figma file, turned into code using Anima.Learn more at AnimaApp.com',
+    image: gema_nolen3,
+    name: ' Gemi Nolen',
+    company: 'Google',
+  },
+]
+
+export const LatestWork = [
+  {
+    image: free_bird,
+    person: 'Lynyrd Skynyrd',
+  },
+  {
+    image: purple_haze,
+    person: 'Jimi Hendrix',
+  },
+  {
+    image: You_really_got_me,
+    person: 'The Kinks',
+  },
+  {
+    image: American_girl,
+    person: 'Tom Petty',
+  },
+  {
+    image: whole_loffa,
+    person: 'Led Zeppelin',
+  },
+  {
+    image: under_pressure,
+    person: 'Queen',
+  },
+]
